40c7132769b798e85b3fa413ea84b1e04bc98959
allyrogge <abr7ct@virginia.edu>
2016-06-07 14:16:39 +0200

    Day7 morning

---
+++ Day7/day7morning.js
@@ -0,0 +1,16 @@
+
+	// console.log the values of First Name and Last Name 
+	function getFormValues() {  
+		var fname= document.getElementById("firstname").value; 
+		var lname=document.getElementById("lastname").value;
+		console.log(fname + " " + lname);
+	}
+
+	// Change the color of the div with id "color-div"
+	function changeColor() {
+		document.getElementById("color-div").style.color = "red"
+
+	}
+
+	
+
---
+++ Day7/day7script.js
@@ -0,0 +1,15 @@
+
+	// console.log the values of First Name and Last Name 
+	function getFormValues() {  
+		var fname= document.getElementById("firstname"); 
+		var lname=document.getElementById("lastname");
+		console.log(fname + " " + lname);
+	}
+
+	// Change the color of the div with id "color-div"
+	function changeColor() {
+
+	}
+
+	
+
